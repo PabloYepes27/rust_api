3d089f48ae4019279c625113812d7219777b5022
Juan Pablo Yepes tamayo <juan.jyepes@mercadolibre.com.co>
2023-12-13 18:35:29 -0500

    GET request with path and query extractors

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,68 @@
-use axum::{routing::get, Router};
+use axum::{
+    body::Body,
+    extract::{Path, Query},
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    routing::{get, post},
+    Json, Router,
+};
+
+use serde::{Serialize, Deserialize};
+
+// A struct for query parameters
+#[derive(Deserialize)]
+struct Page {
+    number: u32,
+}
+
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    name: String,
+    email: String,
+}
+
+// Handler for /create-user
+async fn create_user() -> impl IntoResponse {
+    Response::builder()
+        .status(StatusCode::CREATED)
+        .body(Body::from("User created successfully"))
+        .unwrap()
+}
+// Handler for /users
+async fn list_users() -> Json<Vec<User>> {
+    let users = vec![
+        User {
+            id: 1,
+            name: "Elijah".to_string(),
+            email: "elijah@example.com".to_string(),
+        },
+        User {
+            id: 2,
+            name: "John".to_string(),
+            email: "john@doe.com".to_string(),
+        },
+    ];
+    Json(users)
+}
+
+// A handler to demonstrate path and query extractors
+async fn show_item(Path(id): Path<u32>, Query(page): Query<Page>) -> String {
+    format!("Item {} on page {}", id, page.number)
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, Rust!" }));
+    // Define Routes
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, Rust!" }))
+        .route("/create-user", post(create_user))
+        .route("/item/:id", get(show_item))
+        .route("/users", get(list_users));
 
     println!("Running on http://localhost:3000");
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    // Start Server
+    axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
